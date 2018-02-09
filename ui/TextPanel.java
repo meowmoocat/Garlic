3a6d5a84775fabf3ae50575816b7d682191117dc
Davison129 <anna.davison@ucdconnect.ie>
2018-02-09 11:26:01 +0000

    Added actionListener

--- ui/TextPanel.java
+++ ui/TextPanel.java
@@ -3,6 +3,7 @@ package ui;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -10,47 +11,49 @@ import javax.swing.JTextField;
 
 public class TextPanel extends JPanel
 {
-	
+
 	JTextField textField;
-	
+
 	public TextPanel()
 	{
-		
+
 		textP();
 	}
 
 	private void textP()
 	{
-		
+
 		textField = new JTextField(50);
-		
+
 		textField.setText("");
 
 
-		
+
 		this.add(textField, BorderLayout.SOUTH);
 
 
 		//action listener that moves text to text area when return is pressed
 		textField.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				String enteredText = textField.getText();
+				InfoPanel.textArea.append(enteredText + "\n");
+				textField.setText("");
 			}
 		});
 
 	}
-	
-//	public String getString()
-//	{
-//		String text = textField.getText();
-////		textArea.append(text + "\n");
-////		textField.selectAll();
-////
-////		//Make sure the new text is visible, even if there
-////		//was a selection in the text area.
-////		textArea.setCaretPosition(textArea.getDocument().getLength());
-//		
-//		return text;
-//	}
+
+	//	public String getString()
+	//	{
+	//		String text = textField.getText();
+	////		textArea.append(text + "\n");
+	////		textField.selectAll();
+	////
+	////		//Make sure the new text is visible, even if there
+	////		//was a selection in the text area.
+	////		textArea.setCaretPosition(textArea.getDocument().getLength());
+	//		
+	//		return text;
+	//	}
 
 }
