73e0ed86c4744da86b28d98faff90f8d57e29e43
Orla Keating <orla.keating@ucdconnect.ie>
2018-02-16 09:57:55 +0000

    annas method input??

--- S2/src/garlic/Cluedo.java
+++ S2/src/garlic/Cluedo.java
@@ -3,12 +3,26 @@ package garlic;
 public class Cluedo {
 
 	private void testUI() {
+		
+	}
+	
+	
+	private void inputNames()
+	{
+		{
+		//your stuff here
+		}
+		
+		
+		
 		Moves start = new Moves();
 	}
 
+
 	public static void main(String[] args) {
 		Cluedo game = new Cluedo();
-		game.testUI();
+		
+		game.inputNames();
 		System.exit(0);
 	}
 }
