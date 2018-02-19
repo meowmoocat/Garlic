6547f0e821dff4c49e06e7e4a014a742f9d1b57b
Davison129 <anna.davison@ucdconnect.ie>
2018-02-19 11:48:58 +0000

    finished borders

--- S2/src/garlic/Coordinates.java
+++ S2/src/garlic/Coordinates.java
@@ -263,7 +263,482 @@ public class Coordinates {
 			col=prevCol;
 			row=prevRow;
 		}
-
+		if(prevCol==9 && prevRow==0 && col==9 && row==-1 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==9 && prevRow==0 && col==8 && row==0 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==9 && prevRow==0 && col==10 && row==0 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==9 && prevRow==1 && col==10 && row==1 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==9 && prevRow==1 && col==9 && row==2 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==8 && prevRow==1 && col==8 && row==0 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==8 && prevRow==1 && col==8 && row==2 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==7 && prevRow==1 && col==7 && row==0 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==7 && prevRow==1 && col==6 && row==1 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==7 && prevRow==2 && col==8 && row==2 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==7 && prevRow==3 && col==8 && row==3 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==7 && prevRow==4 && col==8 && row==4 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==7 && prevRow==6 && col==8 && row==6 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==7 && prevRow==7 && col==8 && row==7 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==8 && prevRow==8 && col==8 && row==7 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==10 && prevRow==8 && col==10 && row==7 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==11 && prevRow==8 && col==11 && row==7 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==12 && prevRow==8 && col==12 && row==7 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==13 && prevRow==8 && col==13 && row==7 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==8 && col==15 && row==7 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==7 && col==15 && row==7 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==6 && col==15 && row==6 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==4 && col==15 && row==4 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==3 && col==15 && row==3 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==2 && col==15 && row==2 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==14 && prevRow==0 && col==14 && row==-1 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==14 && prevRow==0 && col==13 && row==0 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==14 && prevRow==0 && col==15 && row==0 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==14 && prevRow==1 && col==13 && row==1 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==14 && prevRow==1 && col==14 && row==2 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==1 && col==15 && row==0 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==1 && col==15 && row==2 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==1 && col==16 && row==0 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==1 && col==17 && row==1 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==17 && prevRow==2 && col==17 && row==1 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==17 && prevRow==2 && col==18 && row==2 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==17 && prevRow==3 && col==18 && row==3 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==17 && prevRow==4 && col==18 && row==4 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==18 && prevRow==5 && col==19 && row==5 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==23 && prevRow==6 && col==23 && row==5 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==23 && prevRow==6 && col==24 && row==6 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==23 && prevRow==6 && col==23 && row==7 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==22 && prevRow==6 && col==22 && row==5 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==21 && prevRow==6 && col==21 && row==5 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==20 && prevRow==6 && col==20 && row==5 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==19 && prevRow==6 && col==19 && row==5 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==22 && prevRow==7 && col==23 && row==7 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==22 && prevRow==7 && col==22 && row==8 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==21 && prevRow==7 && col==21 && row==8 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==20 && prevRow==7 && col==20 && row==8 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==19 && prevRow==7 && col==19 && row==8 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==18 && prevRow==7 && col==18 && row==8 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==17 && prevRow==8 && col==18 && row==8 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==17 && prevRow==10 && col==18 && row==10 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==17 && prevRow==11 && col==18 && row==11 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==17 && prevRow==12 && col==18 && row==12 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==18 && prevRow==13 && col==18 && row==12 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==18 && prevRow==13 && col==18 && row==14 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==19 && prevRow==13 && col==19 && row==12 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==19 && prevRow==13 && col==19 && row==14 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==20 && prevRow==13 && col==20 && row==12 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==21 && prevRow==13 && col==21 && row==12 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==21 && prevRow==13 && col==21 && row==14 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==22 && prevRow==13 && col==22 && row==14 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==22 && prevRow==13 && col==23 && row==13 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==17 && prevRow==14 && col==18 && row==14 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==17 && prevRow==14 && col==17 && row==15 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==15 && col==17 && row==15 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==17 && col==17 && row==17 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==17 && prevRow==18 && col==17 && row==17 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==17 && prevRow==18 && col==18 && row==18 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==23 && prevRow==19 && col==23 && row==18 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==23 && prevRow==19 && col==24 && row==19 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==23 && prevRow==19 && col==23 && row==20 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==22 && prevRow==20 && col==23 && row==20 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==22 && prevRow==20 && col==22 && row==21 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==21 && prevRow==20 && col==21 && row==21 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==20 && prevRow==20 && col==20 && row==21 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==19 && prevRow==20 && col==19 && row==21 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==18 && prevRow==20 && col==18 && row==21 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==21 && col==17 && row==21 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==22 && col==17 && row==22 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==23 && col==17 && row==23 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==24 && col==17 && row==24 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==24 && col==16 && row==25 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==16 && prevRow==24 && col==15 && row==24 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==23 && col==15 && row==24 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==23 && col==14 && row==23 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==22 && col==14 && row==22 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==21 && col==14 && row==21 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==19 && col==14 && row==19 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==18 && col==14 && row==18 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==14 && prevRow==17 && col==14 && row==16 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==14 && prevRow==17 && col==14 && row==18 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==13 && prevRow==17 && col==13 && row==16 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==13 && prevRow==17 && col==13 && row==18 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==11 && prevRow==17 && col==11 && row==16 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==10 && prevRow==17 && col==10 && row==16 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==10 && prevRow==17 && col==10 && row==18 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==9 && prevRow==17 && col==9 && row==18 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==9 && prevRow==16 && col==10 && row==16 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==9 && prevRow==15 && col==10 && row==15 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==9 && prevRow==14 && col==10 && row==14 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==9 && prevRow==13 && col==10 && row==13 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==9 && prevRow==12 && col==10 && row==12 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==9 && prevRow==11 && col==10 && row==11 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==9 && prevRow==10 && col==10 && row==10 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==10 && prevRow==9 && col==10 && row==10 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==11 && prevRow==9 && col==11 && row==10 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==12 && prevRow==9 && col==12 && row==10 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==13 && prevRow==9 && col==13 && row==10 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==14 && prevRow==9 && col==14 && row==10 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==10 && col==14 && row==10 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==11 && col==14 && row==11 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==12 && col==14 && row==12 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==13 && col==14 && row==13 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==14 && col==14 && row==14 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==15 && col==14 && row==15 ) {
+			col=prevCol;
+			row=prevRow;
+		}
+		if(prevCol==15 && prevRow==16 && col==14 && row==16 ) {
+			col=prevCol;
+			row=prevRow;
+		}
 
 	}
 
