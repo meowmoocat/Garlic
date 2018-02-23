4dc1bb089d49269c4b57ffee5fbf022ee58fd799
Davison129 <anna.davison@ucdconnect.ie>
2018-02-23 11:09:49 +0000

    added refresh

--- S2/src/garlic/InfoPanel.java
+++ S2/src/garlic/InfoPanel.java
@@ -6,28 +6,32 @@ import java.awt.*;
 
 public class InfoPanel extends JPanel {
 
-	private static final long serialVersionUID = 1L;
-	private static final int TEXT_AREA_HEIGHT = 40;
-	private static final int CHARACTER_WIDTH = 60;
-	private static final int FONT_SIZE = 12;
+    private static final long serialVersionUID = 1L;
+    private static final int TEXT_AREA_HEIGHT = 40;
+    private static final int CHARACTER_WIDTH = 60;
+    private static final int FONT_SIZE = 12;
 
-	private final JTextArea textArea  = new JTextArea(TEXT_AREA_HEIGHT, CHARACTER_WIDTH);
+    private final JTextArea textArea  = new JTextArea(TEXT_AREA_HEIGHT, CHARACTER_WIDTH);
 
-	InfoPanel() {
-		JScrollPane scrollPane = new JScrollPane(textArea);
-		DefaultCaret caret = (DefaultCaret)textArea.getCaret();
-		textArea.setEditable(false);
-		textArea.setFont(new Font("monospaced", Font.PLAIN, FONT_SIZE));
-		textArea.setLineWrap(true);
-		textArea.setWrapStyleWord(true);
-		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-		setLayout(new BorderLayout());
-		add(scrollPane, BorderLayout.CENTER);
-	}
+    InfoPanel() {
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        DefaultCaret caret = (DefaultCaret)textArea.getCaret();
+        textArea.setEditable(false);
+        textArea.setFont(new Font("monospaced", Font.PLAIN, FONT_SIZE));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        setLayout(new BorderLayout());
+        add(scrollPane, BorderLayout.CENTER);
+    }
 
-	public void addText(String text) {
-		textArea.setText(textArea.getText()+"\n"+text);
-	}
+    public void addText(String text) {
+        textArea.setText(textArea.getText()+"\n"+text);
+    }
+
+	public void refresh() {
+		textArea.setText("");
+    }
 
 }
