bb38d66cdbeec4718c999ff5ecfd0278dbb9c245
Davison129 <anna.davison@ucdconnect.ie>
2018-02-24 15:16:31 +0000

    added comments

--- S2/src/garlic/CommandPanel.java
+++ S2/src/garlic/CommandPanel.java
@@ -1,5 +1,11 @@
 package garlic;
 
+/* created by
+ * Anna Davison	16382333
+ * James Kearns	15467622
+ * Orla Keating	15205679
+ */
+
 import java.awt.event.ActionEvent;
 import java.util.*;
 import java.awt.event.ActionListener;
@@ -8,42 +14,44 @@ import java.awt.*;
 
 public class CommandPanel extends JPanel  {
 
-	private static final long serialVersionUID = 1L;
-	private static final int FONT_SIZE = 14;
+    private static final long serialVersionUID = 1L;
+    private static final int FONT_SIZE = 14;
 
-	private final JTextField commandField = new JTextField();
-	private final LinkedList<String> commandBuffer = new LinkedList<>();
+    private final JTextField commandField = new JTextField();
+    private final LinkedList<String> commandBuffer = new LinkedList<>();
 
-	CommandPanel() {
-		class AddActionListener implements ActionListener {
-			public void actionPerformed(ActionEvent event)	{
-				synchronized (commandBuffer) {
-					commandBuffer.add(commandField.getText());
-					commandField.setText("");
-					commandBuffer.notify();
-				}
-			}
-		}
-		ActionListener listener = new AddActionListener();
-		commandField.addActionListener(listener);
-		commandField.setFont(new Font("Times New Roman", Font.PLAIN, FONT_SIZE));
-		setLayout(new BorderLayout());
-		add(commandField, BorderLayout.CENTER);
-	}
+    //adds entered text to commandBuffer
+    CommandPanel() {
+        class AddActionListener implements ActionListener {
+            public void actionPerformed(ActionEvent event)	{
+                synchronized (commandBuffer) {
+                    commandBuffer.add(commandField.getText());
+                    commandField.setText("");
+                    commandBuffer.notify();
+                }
+            }
+        }
+        ActionListener listener = new AddActionListener();
+        commandField.addActionListener(listener);
+        commandField.setFont(new Font("Times New Roman", Font.PLAIN, FONT_SIZE));
+        setLayout(new BorderLayout());
+        add(commandField, BorderLayout.CENTER);
+    }
 
-	public String getCommand() {
-		String command;
-		synchronized(commandBuffer) {
-			while (commandBuffer.isEmpty()) {
-				try {
-					commandBuffer.wait();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			command = commandBuffer.pop();
-		}
-		return command;
-	}
+    //gets input from user
+    public String getCommand() {
+        String command;
+        synchronized(commandBuffer) {
+            while (commandBuffer.isEmpty()) {
+                try {
+                    commandBuffer.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            command = commandBuffer.pop();
+        }
+        return command;
+    }
 
 }
