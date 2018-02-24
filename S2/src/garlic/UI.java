3a8afc6c15f599bdb865492805247c52a2bcbb54
Davison129 <anna.davison@ucdconnect.ie>
2018-02-24 16:22:05 +0000

    added comments

--- S2/src/garlic/UI.java
+++ S2/src/garlic/UI.java
@@ -1,43 +1,54 @@
 package garlic;
 
+/* created by
+ * Anna Davison	16382333
+ * James Kearns	15467622
+ * Orla Keating	15205679
+ */
+
 import java.awt.BorderLayout;
 import javax.swing.*;
 
 public class UI {
 
-	private static final int FRAME_WIDTH = 1095;
-	private static final int FRAME_HEIGHT = 700;
+    private static final int FRAME_WIDTH = 1200;
+    private static final int FRAME_HEIGHT = 800;
 
-	private final BoardPanel boardPanel;
-	private final InfoPanel infoPanel = new InfoPanel();
-	private final CommandPanel commandPanel = new CommandPanel();
+    private final BoardPanel boardPanel;
+    private final InfoPanel infoPanel = new InfoPanel();
+    private final CommandPanel commandPanel = new CommandPanel();
 
-	UI(Tokens characters, Weapons weapons) {
-		JFrame frame = new JFrame();
-		boardPanel = new BoardPanel(characters, weapons);
-		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-		frame.setTitle("UCD Cluedo");
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.add(boardPanel, BorderLayout.LINE_START);
-		frame.add(infoPanel, BorderLayout.LINE_END);
-		frame.add(commandPanel,BorderLayout.PAGE_END);
-		frame.setResizable(false);
-		frame.setVisible(true);
-	}
+    //adds boardPanel, infoPanel and commandPanel to jFrame in correct board layout
+    UI(Tokens characters, Weapons weapons) {
+        JFrame frame = new JFrame();
+        boardPanel = new BoardPanel(characters, weapons);
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        frame.setTitle("Cluedo");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(boardPanel, BorderLayout.LINE_START);
+        frame.add(infoPanel, BorderLayout.LINE_END);
+        frame.add(commandPanel,BorderLayout.PAGE_END);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
 
-	public String getCommand() {
-		return commandPanel.getCommand();
-	}
+    //get command form commandPanel
+    public String getCommand() {
+        return commandPanel.getCommand();
+    }
 
-	public void display() {
-		boardPanel.refresh();
-	}
+    //redraws board
+    public void display() {
+        boardPanel.refresh();
+    }
+    
+    //emptys infoPanel
+    public void refreshInfoPanel() {
+    	infoPanel.refresh();
+    }
 
-	public void refreshInfoPanel() {
-		infoPanel.refresh();
-	}
-
-	public void displayString(String string) {
-		infoPanel.addText(string);
-	}
+    //adds string to infoPanel
+    public void displayString(String string) {
+        infoPanel.addText(string);
+    }
 }
