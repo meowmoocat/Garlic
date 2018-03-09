385e3538a7a539a4c25b94aef8f44e77cda44ee7
Davison129 <anna.davison@ucdconnect.ie>
2018-03-09 09:38:39 +0000

    fixed superclass

--- S3/Cards.java
+++ S3/Cards.java
@@ -3,18 +3,20 @@ package S3;
 public class Cards {
 	//superclass of all cards
 	
-	private static int cardNum=0;
+	public static int cardNum;
+	public static String cardName;
 	
-	Cards() {
-		allocateCards();
+	public Cards(int cardNum, String cardName) {
+		this.cardNum=cardNum++;
+		this.cardName=cardName;
+	}
+	
+	public int getCardNum(){
+		return cardNum;
+	}
+	
+	public String getCardName(){
+		return cardName;
 	}
 
-	private void allocateCards() {
-				
-		String[] TOKENS = {"Bleakley Plum", "Kelly White", "Caka Scarlett", "Sweeney Green", "Kalra Mustard", "Delorey Peacock"};
-		String[] WEAPONS = {"Book", "Bored", "Gradcap", "Librocop", "Microscope", "Seagull"};
-		String[] ROOMS = {"Computer Science", "O'Reilly Hall", "Engineering", "Sutherland", "Quinn",
-				"Newman", "Library", "Ag Science", "O'Brien", "Lake"};
-		
-	}
 }
