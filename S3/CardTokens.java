ebb64ebadb42c43de0c99879c5e21c6c17072cd8
Davison129 <anna.davison@ucdconnect.ie>
2018-03-09 12:53:05 +0000

    get method

--- S3/CardTokens.java
+++ S3/CardTokens.java
@@ -1,10 +1,14 @@
-package S3;
-
 public class CardTokens extends Cards{
 	
 	private String type = "token";
+	public String tokenName;
 
 	public CardTokens(String name) {
 		super(name);
+		this.tokenName=name;
+	}
+	
+	public String getTokenName() {
+		return tokenName;
 	}
 }
