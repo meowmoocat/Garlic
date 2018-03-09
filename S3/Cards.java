5c0a02422eedc16702f4b1cac294292da9df302e
Davison129 <anna.davison@ucdconnect.ie>
2018-03-09 09:27:47 +0000

    start of cards

--- S3/Cards.java
+++ S3/Cards.java
@@ -1,5 +1,20 @@
 package S3;
 
 public class Cards {
+	//superclass of all cards
+	
+	private static int cardNum=0;
+	
+	Cards() {
+		allocateCards();
+	}
 
+	private void allocateCards() {
+				
+		String[] TOKENS = {"Bleakley Plum", "Kelly White", "Caka Scarlett", "Sweeney Green", "Kalra Mustard", "Delorey Peacock"};
+		String[] WEAPONS = {"Book", "Bored", "Gradcap", "Librocop", "Microscope", "Seagull"};
+		String[] ROOMS = {"Computer Science", "O'Reilly Hall", "Engineering", "Sutherland", "Quinn",
+				"Newman", "Library", "Ag Science", "O'Brien", "Lake"};
+		
+	}
 }
