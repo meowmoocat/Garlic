45e9a74741a1be18e00d8cc9de3f2460db1be741
Davison129 <anna.davison@ucdconnect.ie>
2018-03-09 12:53:48 +0000

    get method

--- S3/CardWeapons.java
+++ S3/CardWeapons.java
@@ -3,8 +3,14 @@ package S3;
 public class CardWeapons extends Cards{
 	
 	private String type = "weapon";
+	public String weaponName;
 
 	public CardWeapons(String name) {
 		super(name);
+		this.weaponName=name;
+	}
+	
+	public String getWeaponName() {
+		return weaponName;
 	}
 }
