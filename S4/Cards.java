ec40499646ffa0fc9947bca69d769d31833c845c
Davison129 <anna.davison@ucdconnect.ie>
2018-04-07 16:01:28 +0100

    comments

--- S4/Cards.java
+++ S4/Cards.java
@@ -1,5 +1,11 @@
 package S4;
 
+/* created by Garlic
+ * Anna Davison	16382333
+ * James Kearns	15467622
+ * Orla Keating	15205679
+ */
+
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -23,7 +29,7 @@ public class Cards {
 					return true;
 				} 
 			} catch (Exception e) {
-				// TODO: handle exception
+				//handle exception
 			}
 		} 
 		return false;
