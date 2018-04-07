2a5dbefd671ebb6d86b55ef95089e3280abbd6b4
Davison129 <anna.davison@ucdconnect.ie>
2018-04-07 15:07:45 +0100

    comments

--- S4/Weapon.java
+++ S4/Weapon.java
@@ -1,5 +1,11 @@
 package S4; 
 
+/* created by Garlic
+ * Anna Davison	16382333
+ * James Kearns	15467622
+ * Orla Keating	15205679
+ */
+
 public class Weapon {
 
 	private final String name;
@@ -29,15 +35,16 @@ public class Weapon {
 		return this.name.toLowerCase().contains(name.toLowerCase().trim());
 	}
 
+	//puts weapon in the room
 	public void enterRoom(Room room) {
 		this.room = room;
 		position = this.room.addWeapon();
 	}
-	
+
 	public Room getRoom() {
 		return room;
 	}
-	
+
 	public void setRoom(Room room)
 	{
 		this.room = room;
