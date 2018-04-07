6f62f72e4120b6a36b9f12f56f82431dfabd12b5
Davison129 <anna.davison@ucdconnect.ie>
2018-04-07 15:59:01 +0100

    comments

--- S4/CommandPanel.java
+++ S4/CommandPanel.java
@@ -1,5 +1,11 @@
 package S4;
 
+/* created by Garlic
+ * Anna Davison	16382333
+ * James Kearns	15467622
+ * Orla Keating	15205679
+ */
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
