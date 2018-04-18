46b76f99e7a817391120d6c0e42a220ad6d3bf42
Davison129 <anna.davison@ucdconnect.ie>
2018-04-18 18:44:18 +0100

    update

--- src/gameengine/Weapon.java
+++ src/gameengine/Weapon.java
@@ -2,31 +2,31 @@ package gameengine;
 
 public class Weapon {
 
-	private final String name;
-	private Coordinates position;
-	private Room currentRoom;
+    private final String name;
+    private Coordinates position;
+    private Room currentRoom;
 
-	Weapon (String name, Room room) {
-		this.name = name;
-		currentRoom = room;
-		position = currentRoom.addItem();
-	}
+    Weapon (String name, Room room) {
+        this.name = name;
+        currentRoom = room;
+        position = currentRoom.addItem();
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Coordinates getPosition() {
-		return position;
-	}
+    public Coordinates getPosition() {
+        return position;
+    }
 
-	void changeRoom(Room room) {
-		currentRoom.removeItem(position);
-		currentRoom = room;
-		position = currentRoom.addItem();
-	}
+    void changeRoom(Room room) {
+        currentRoom.removeItem(position);
+        currentRoom = room;
+        position = currentRoom.addItem();
+    }
 
-	public boolean hasName (String name) {
-		return this.name.toLowerCase().trim().equals(name.toLowerCase().trim());
-	}
+    public boolean hasName (String name) {
+        return this.name.toLowerCase().trim().equals(name.toLowerCase().trim());
+    }
 }
