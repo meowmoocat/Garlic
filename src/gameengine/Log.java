f1a61fde7018e3b510b33d537b39f12bb6ef6e94
meowmoocat <orla.keating@ucdconnect.ie>
2018-04-19 12:08:32 +0100

    TAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBSSSSSSSSSSSSSSSSSSSSSSSSSS........................

--- src/gameengine/Log.java
+++ src/gameengine/Log.java
@@ -8,39 +8,39 @@ import java.util.Iterator;
 
 public class Log implements Iterable<String>, Iterator<String> {
 
-    private final ArrayList<String> messages = new ArrayList<>();
-    private Iterator<String> iterator;
+	private final ArrayList<String> messages = new ArrayList<>();
+	private Iterator<String> iterator;
 
-    void addExchange(Player currentPlayer, Player queriedPlayer, Query query, boolean cardFound) {
-        messages.add(currentPlayer + " questioned " + queriedPlayer + " about " + query.getSuspect() + " with the "
-                + query.getWeapon() + " in the " + query.getRoom() + ".");
-        if (cardFound) {
-            messages.add(queriedPlayer + " showed one card.");
-        } else {
-            messages.add(queriedPlayer + " did not show any cards.");
-        }
-    }
+	void addExchange(Player currentPlayer, Player queriedPlayer, Query query, boolean cardFound) {
+		messages.add(currentPlayer + " questioned " + queriedPlayer + " about " + query.getSuspect() + " with the "
+				+ query.getWeapon() + " in the " + query.getRoom() + ".");
+		if (cardFound) {
+			messages.add(queriedPlayer + " showed one card.");
+		} else {
+			messages.add(queriedPlayer + " did not show any cards.");
+		}
+	}
 
-    void addExchange(Player currentPlayer, Player queriedPlayer, Query query, Card card) {
-        messages.add(currentPlayer + " questioned " + queriedPlayer + " about " + query.getSuspect() + " with the "
-                + query.getWeapon() + " in the " + query.getRoom() + ".");
-        messages.add(queriedPlayer + " showed one card: " + card + ".");
-    }
+	void addExchange(Player currentPlayer, Player queriedPlayer, Query query, Card card) {
+		messages.add(currentPlayer + " questioned " + queriedPlayer + " about " + query.getSuspect() + " with the "
+				+ query.getWeapon() + " in the " + query.getRoom() + ".");
+		messages.add(queriedPlayer + " showed one card: " + card + ".");
+	}
 
-    public boolean isEmpty() {
-        return messages.size()==0;
-    }
+	public boolean isEmpty() {
+		return messages.size()==0;
+	}
 
-    public boolean hasNext() {
-        return iterator.hasNext();
-    }
+	public boolean hasNext() {
+		return iterator.hasNext();
+	}
 
-    public String next() {
-        return iterator.next();
-    }
+	public String next() {
+		return iterator.next();
+	}
 
-    public Iterator<String> iterator() {
-        iterator = messages.iterator();
-        return iterator;
-    }
+	public Iterator<String> iterator() {
+		iterator = messages.iterator();
+		return iterator;
+	}
 }
